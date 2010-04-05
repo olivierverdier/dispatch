372287c865d6956eae1e152c17b7425c7ea87aad
Olivier Verdier <olivier.verdier@gmail.com>
2010-04-05 09:54:25 +0200

    put into distributable package form

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'dispatch',
+    description = 'Dispatch package for python',
+    license = 'BSD',
+    keywords = ['python', 'dispatch',],
+    
+    packages=['dispatch'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    zip_safe = False,
+    )
