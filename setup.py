2e9d8f19e7a3bcd9bd5e0ed76efc9fe840c12239
Raf Guns <rafguns@gmail.com>
2011-11-17 11:52:01 +0100

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
 from distutils.core import setup
 
 setup(
-    name = 'dispatch',
-    description = 'Dispatch package for python',
-    license = 'BSD',
-    keywords = ['python', 'dispatch',],
-    
-    packages=['dispatch'],
+    name = 'Dispatch',
+    description = 'A library for event-driven programming',
+    packages = ['dispatch'],
     classifiers = [
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules"],
-    zip_safe = False,
-    )
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
