364d30b4440b5213edc9986c636e216d15c1af58
Philip Horger <campadrenalin@gmail.com>
2013-08-16 22:25:37 -0700

    Reorganize Saferef tests to test individual situations

--- dispatch/tests/test_saferef.py
+++ dispatch/tests/test_saferef.py
@@ -16,52 +16,58 @@ class Test2(object):
     def __call__(self, obj):
         pass
 
-class Tester(unittest.TestCase):
+class TestSaferefForTest1(unittest.TestCase):
     def setUp(self):
-        ts = []
-        ss = []
+        self.sources = []
+        self.refs = []
         self.closureCount = 0
-        for x in range(5000):
-            t = Test1()
-            ts.append(t)
-            s = safeRef(t.x, self._closure)
-            ss.append(s)
-        ts.append(dummy)
-        ss.append(safeRef(dummy, self._closure))
-        for x in range(30):
-            t = Test2()
-            ts.append(t)
-            s = safeRef(t, self._closure)
-            ss.append(s)
-        self.ts = ts
-        self.ss = ss
+        for x in range(self.count):
+            source   = self.get_source()
+            referant = self.get_referant(source)
+            ref = safeRef(referant, self._closure)
+
+            self.sources.append(source)
+            self.refs.append(ref)
     
     def tearDown(self):
-        del self.ts
-        del self.ss
+        del self.sources
+        del self.refs
+
+    def get_source(self):
+        return Test1()
+
+    def get_referant(self, source):
+        return source.x
+
+    @property
+    def count(self):
+        return 5 # 5000
     
     def testIn(self):
         """Test the "in" operator for safe references (cmp)"""
-        for t in self.ts[:50]:
-            self.assertIn(safeRef(t.x), self.ss)
+        for source in self.sources[:50]:
+            self.assertIn(
+                safeRef(self.get_referant(source)),
+                self.refs
+            )
     
     def testValid(self):
         """Test that the references are valid (return instance methods)"""
-        for s in self.ss:
-            self.assertTrue(s())
+        for ref in self.refs:
+            self.assertTrue(ref())
     
     def testShortCircuit (self):
         """Test that creation short-circuits to reuse existing references"""
         sd = {}
-        for s in self.ss:
-            sd[s] = 1
-        for t in self.ts:
-            if hasattr(t, 'x'):
-                self.assertTrue(sd.has_key(safeRef(t.x)))
-                self.assertTrue(safeRef(t.x) in sd)
+        for ref in self.refs:
+            sd[ref] = 1
+        for source in self.sources:
+            if hasattr(source, 'x'):
+                #self.assertTrue(sd.has_key(safeRef(source.x)))
+                self.assertTrue(safeRef(source.x) in sd)
             else:
-                self.assertTrue(sd.has_key(safeRef(t)))
-                self.assertTrue(safeRef(t) in sd)
+                #self.assertTrue(sd.has_key(safeRef(source)))
+                self.assertTrue(safeRef(source) in sd)
     
     def testRepresentation (self):
         """Test that the reference object's representation works
@@ -69,14 +75,44 @@ class Tester(unittest.TestCase):
         XXX Doesn't currently check the results, just that no error
             is raised
         """
-        repr(self.ss[-1])
+        repr(self.refs[-1])
         
     def _closure(self, ref):
         """Dumb utility mechanism to increment deletion counter"""
         self.closureCount +=1
 
+class TestSaferefForTest2(TestSaferefForTest1):
+    def get_source(self):
+        return Test2()
+
+    def get_referant(self, source):
+        return source
+
+    @property
+    def count(self):
+        return 3 # 30
+
+class TestSaferefForDummy(TestSaferefForTest1):
+    def get_source(self):
+        return dummy
+
+    def get_referant(self, source):
+        return source
+
+    @property
+    def count(self):
+        return 1
+
 def getSuite():
-    return unittest.makeSuite(Tester,'test')
+    cases = [
+        TestSaferefForTest1,
+        TestSaferefForTest2,
+        TestSaferefForDummy,
+    ]
+    suite = unittest.TestSuite()
+    for case in cases:
+        suite.addTests(unittest.loadTestsFromTestCase(case))
+    return suite
 
 if __name__ == "__main__":
     unittest.main()
