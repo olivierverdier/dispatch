caabbf6dffd07f4e0d28b157eb1d8287e57763e7
Olivier Verdier <olivier.verdier@gmail.com>
2013-11-12 10:17:38 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name = 'Dispatch',
+    name = 'dispatcher',
+    maintainer = 'Olivier Verdier',
+    maintainer_email = 'olivier.verdier@gmail.com',
     description = 'A library for event-driven programming',
     packages = [
         'dispatch',
         'dispatch.tests',
     ],
+    version = '1.0',
+    url = 'https://github.com/olivierverdier/dispatch',
     classifiers = [
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
